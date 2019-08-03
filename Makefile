96ff6300718fac88ba868e90097f415aad00ec35
Stephen Kelly <stevekm@users.noreply.github.com>
2019-08-03 00:02:52 -0400

    add field arg parsing

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
 SHELL:=/bin/bash
+rustup:
+	curl https://sh.rustup.rs -sSf | sh
+
+update:
+	rustup update
+	rustup self update
+
 run:
 	printf 'foo1\tfoo2\tfoo3\nbar1\tbar2\tbar3\n' | cargo run
 	cargo run data.tsv
--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,45 @@ impl Reader {
     }
 }
 
-fn split_fields(fields_str: &str) -> Vec<& u32> {
-    let mut fields = Vec::new();
-    fields
-}
-
 fn parse_int(number_str: &str) -> u32 {
+    // convert string to int
     let number = number_str.parse::<u32>().expect(format!("Failed to parse {}", number_str).as_str());
     number
 }
 
 
+fn get_fields(fields_str: &str) -> Vec<u32> {
+    // parse out the field indexes from the CLI arg
+    let mut values = Vec::new();
+
+    // first split comma separated components
+    let fields_parts = fields_str.split(",").collect::<Vec<&str>>();
+    for part in fields_parts{
+        if part.contains("-"){
+            // check for '-' indicating a range
+            let subparts = part.split("-").collect::<Vec<&str>>();
+            if subparts.len() != 2 {
+                panic!("Could not parse '{}'", part);
+            }
+            let start = parse_int(&subparts[0]);
+            let mut end = parse_int(&subparts[1]);
+            end = end + 1;
+            for num in (std::ops::Range {start: start, end: end}){
+                values.push(num);
+            }
+        } else {
+            let parsed = parse_int(&part);
+            values.push(parsed);
+        }
+
+    }
+
+    values
+}
+
+
 fn split_line<'a>(line: &'a str, delimiter: &'a str) -> Vec<&'a str> {
+    // split lines of text from the input file
     let output = line.split(delimiter).collect();
     output
 }
@@ -53,16 +80,14 @@ fn main()  {
     let reader = Reader { input: inputFile.to_string() };
 
     for line in reader.get().lines() {
-        println!("{:?}", line);
+        match line {
+            Ok(l) => {
+                let parts = split_line(&l, delimiter);
+                println!("{:?}", parts);
+            }
+            Err(e) => println!("error parsing line: {:?}", e),
+        }
     }
-    // if vec![1] == vec![0] {
-    //     println!("yes");
-    // } else {
-    //     println!("no");
-    // }
-
-    // let number = parse_int("100e");
-    // println!("{}", number)
 }
 
 
@@ -109,16 +134,36 @@ mod tests {
         assert_eq!(output, expected_output)
     }
 
-    // #[test]
-    // #[should_panic]
-    // fn parse_bad_int(){
-    //     parse_int("100e")
-    // }
+    #[test]
+    fn test_get_fields_0(){
+        let fields_str = "0";
+        let expected_output = vec![0];
+        let output = get_fields(fields_str);
+        assert_eq!(output, expected_output)
+    }
 
-    // fn split_fields1() {
-    //     let input = "1";
-    //     let output = split_fields(input);
-    //     let expected_output = vec![1];
-    //     assert_eq!(output, expected_output);
-    // }
+    #[test]
+    fn test_get_fields_multi(){
+        let fields_str = "1,2";
+        let expected_output = vec![1,2];
+        let output = get_fields(fields_str);
+        assert_eq!(output, expected_output)
+    }
+
+    #[test]
+    fn test_get_fields_range(){
+        let fields_str = "1-3";
+        let expected_output = vec![1,2,3];
+        let output = get_fields(fields_str);
+        assert_eq!(output, expected_output)
+    }
+
+    #[test]
+    fn test_get_fields_range_multi(){
+        let fields_str = "1-3,4,7,9-12";
+        let expected_output = vec![1,2,3,4,7,9,10,11,12];
+        let output = get_fields(fields_str);
+        assert_eq!(output, expected_output)
+    }
+
 }
